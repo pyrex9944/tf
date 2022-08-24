a07bdfffc9482aaa7dc4d5b2e001734954632ea5
Ashirov Sirojiddin <iprochechya@gmail.com>
2022-08-24 23:10:18 +0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.region
-  access_key = "AKIAS2RINRSEBSFB4X7B"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
